38ec8bb3ae48cda980bead9894a334c20e88f853
Malapad <155622018+Malapad@users.noreply.github.com>
2024-01-11 14:35:20 +0800

    Add files via upload

---
+++ James_form.hpp
@@ -0,0 +1,35 @@
+<html>
+ <head>
+  <title>Form</title>
+ </head>
+ <body>
+  <form>
+   <label>First Name:</label>
+   <input type="text"><br><br>
+   
+   <label>Middle Name:</label>
+   <input type="text"><br><br>
+   
+   <label>Last Name:</label>
+   <input type="text"><br><br>
+   
+   <label>Email:</label>
+   <input type="text"><br><br>
+   
+   <label>Gender:</label>
+   <select name="">
+    <option value="Male">Male</option>
+    <option value="Female">Female</option>
+   </select><br><br>
+   
+   <label>Subjects Taken:</label><br>
+   <input type="checkbox">Math<br>
+   <input type="checkbox">Science<br>
+   <input type="checkbox">Filipino<br>
+   <input type="checkbox">English<br><br>
+   
+   <input type="reset" value="Reset">
+   <input type="submit" value="Submit">
+  </form>
+ </body>
+</html>
